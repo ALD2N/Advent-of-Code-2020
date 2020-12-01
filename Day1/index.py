b6330bb1c58d9dfe45a62dda8dfe4589c0dd4764
Cattoen Axel <axel14022001@gmail.com>
2020-12-01 13:40:22 +0100

    Create index.py

---
+++ Day1/index.py
@@ -0,0 +1,8 @@
+file = open("source1.txt","r")
+liste = file.readlines()
+for i in range(len(liste)):
+    liste[i] = int(liste[i])
+for i in range(len(liste)):
+    for y in range(i+1,len(liste)-1):
+        if liste[i]+liste[y] == 2020:
+            print(liste[i]*liste[y])
